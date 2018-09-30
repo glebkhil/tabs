52f66a3b95b159dc94a38f3c71d7eaca53bad44c
Boris Levitan <tab@firemail.cc>
2018-09-30 13:28:23 +0300

    ./upload.sh

--- jobs/update_proxies.rb
+++ jobs/update_proxies.rb
@@ -2,15 +2,19 @@ require_relative './requires'
 logger = CronLogger.new
 
 logger.noise "Updating proxies ... "
-proxies = "181.215.12.172:3954:user12772:79xb0c
-181.215.26.47:3954:user12772:79xb0c
-191.96.17.227:3954:user12772:79xb0c
-181.214.0.124:3954:user12772:79xb0c
-181.215.12.153:3954:user12772:79xb0c
+proxies = "5.188.32.189:9285
+5.188.33.246:9285
+5.188.33.98:9285
+5.188.33.168:9285
+5.188.33.217:9285
+5.188.33.93:9285
+5.188.32.249:9285
+5.188.32.21:9285
+5.188.33.85:9285
+5.188.32.231:9285
 ".split("\n")
 
 proxies.each do |proxy_string|
-
   proxy = proxy_string.split(":")
   Prox.create(host: proxy[0], port: proxy[1], status: Prox::ONLINE, login: 'user12772', password: '79xb0c', provider: "proxio")
   logger.say("Proxy #{proxy.first}:#{proxy.last} added to proxy pool")
