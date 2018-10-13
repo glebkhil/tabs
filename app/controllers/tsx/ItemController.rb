a0573146219b6aaf3305b92456c88bfe225f21d2
Boris Levitan <mmbug256@gmail.com>
2018-10-13 14:00:01 +0300

    ./upload.sh

--- app/controllers/tsx/ItemController.rb
+++ app/controllers/tsx/ItemController.rb
@@ -3,13 +3,12 @@ module TSX
     module Public
 
       def add_item
-        not_permitted if !hb_client.is_admin?(@tsx_bot) or !hb_client.is_kladman?(@tsx_bot)
+        not_permitted if !hb_client.is_admin?(@tsx_bot) and !hb_client.is_kladman?(@tsx_bot)
         handle('save_item')
         reply_simple "meine/item_photo"
       end
 
       def save_item(data = nil)
-        not_permitted if !hb_client.is_admin?(@tsx_bot) or !hb_client.is_kladman?(@tsx_bot)
         fil = @bot.api.get_file(file_id: file?)
         blue fil.inspect
         klad_url = "https://api.telegram.org/file/bot#{@tsx_bot.token}/#{fil['result']['file_path']}"
@@ -22,7 +21,6 @@ module TSX
       end
 
       def save_desc(data)
-        not_permitted if !hb_client.is_admin?(@tsx_bot) or !hb_client.is_kladman?(@tsx_bot)
         sset('tsx_new_item_desc', @payload.text)
         prod = sget('tsx_filter_product')
         qns = Price.where(bot: @tsx_bot.id, product: prod.id)
@@ -31,7 +29,6 @@ module TSX
       end
 
       def save_location
-        not_permitted if !hb_client.is_admin?(@tsx_bot) or !hb_client.is_kladman?(@tsx_bot)
         sset('tsx_new_item_long', @payload.location.longitude)
         sset('tsx_new_item_lat', @payload.location.latitude)
         reply_simple 'meine/item_desc'
@@ -39,7 +36,6 @@ module TSX
       end
 
       def save_completely(data = nil)
-        not_permitted if !hb_client.is_admin?(@tsx_bot) or !hb_client.is_kladman?(@tsx_bot)
         d = sget('tsx_filter')
         p = sget('tsx_filter_product')
         pric = Price.find(qnt: @payload.text.split(' ').first, product: sget('tsx_filter_product').id, bot: @tsx_bot.id)
