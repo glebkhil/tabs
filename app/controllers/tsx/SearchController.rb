bcce666952bd0fa924e3b93bc98b50454a0e4919
Boris Levitan <mmbug256@gmail.com>
2018-10-14 17:29:04 +0300

    ./upload.sh

--- app/controllers/tsx/SearchController.rb
+++ app/controllers/tsx/SearchController.rb
@@ -291,7 +291,7 @@ module TSX
           botrec('Выбран метод оплаты', data)
           buts = _trade.confirmation_buttons(hb_client, data)
           reply_update 'search/trade', ben: seller_bot.beneficiary, seller_bot: seller_bot, seller: seller, method: data, ch: @tsx_bot.is_chief?
-          answer_callback "Метод оплаты изменен."
+          answer_callback "Метод оплаты изменен.", buts
         end
       end
     end
