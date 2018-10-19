355730b86ab93bdab136d3ab3d157b9f6116e59d
Boris Levitan <mmbug256@gmail.com>
2018-10-19 09:07:32 +0300

    Version bump to 10.0.5649

--- app/models/games.rb
+++ app/models/games.rb
@@ -1,15 +1,18 @@
-class Plugins
+class Games
   attr_accessor :bot
 
   def initialize(bot)
     @bot = bot
   end
 
-  def choose_one
+  def self.choose_one
     p = Gameplay.
+      join(:plugin, :plugin__id => :game__plugin).
       where(:bot => @bot.id, :status => Gameplay::ACTIVE).
+      exclude(:plugin__job => Gameplay::JOB).
       order(Sequel.desc(:game__last_run)).limit(1).first
     p.update(:last_run => Date.now)
+    p
   end
 
 end
