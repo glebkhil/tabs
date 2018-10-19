e80105fc4a3fa306184887c9a01c5cf6762f955f
Boris Levitan <mmbug256@gmail.com>
2018-10-19 04:57:25 +0300

    Version bump to 10.0.5419

---
+++ app/models/plugin.rb
@@ -0,0 +1,39 @@
+class Gameplay < Sequel::Model(:game)
+  ACTIVE = 1
+  INACTIVE = 0
+  GAMEOVER = 3
+
+  def readable_status
+    case self.status
+      when Gameplay::ACTIVE
+        "активен"
+      when Gameplay::INACTIVE
+        "неактивен"
+      when Gameplay::GAMEOVER
+        "завершена"
+    end
+  end
+
+  def available_numbers
+    rng = eval("#{self.conf('range')}")
+    nums = []
+    b = Bot[self.bot]
+    Bet.where(game: self.id).each do |num|
+      nums.push(num.number)
+    end
+    puts "NUMBERS LEFT".colorize(:yellow)
+    puts (rng - nums).inspect
+    rng - nums
+  end
+
+  def over!
+    self.status = self::INACTIVE
+    self.save
+  end
+
+  def conf(key)
+    params = JSON.parse(self.config)
+    params[key] || 'unknown'
+  end
+
+end
