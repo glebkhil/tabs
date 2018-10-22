b7330e45bce48d6c0c2975e5e5b4ad80a355de2c
Boris Levitan <mmbug256@gmail.com>
2018-10-22 16:09:41 +0300

    sales

--- jobs/set_sales.rb
+++ jobs/set_sales.rb
@@ -9,7 +9,7 @@ logger.noise "Setting sales ... "
 
 sls = Vars.where('today_sales is not null and today_sales > 0').order(Sequel.desc(:today_sales))
 max_sales = sls.first
-apteka = rand((max_sales.today_sales.to_f - 10).round.to_i..max_sales.today_sales.to_i-4)
+apteka = max_sales.today_sales - rand(20..30)
 logger._say "Max sales today ... "
 logger.answer "#{max_sales.today_sales}", :green
 logger._say'APTEKA TODAY SALES: '
@@ -17,15 +17,10 @@ logger.answer "#{apteka}", :green
 bots = Bot.all
 bots.each do |c|
   begin
-    today_cnt = c.today_bot_sales(Date.today)
+    today_cnt = c.today_bot_sales(Date.today - 1.day)
     cnt = c.sales
     logger.noise "Today_sales=#{today_cnt}, sales=#{cnt} for #{c.tele}"
     c.set_var('sales', c.sales)
-    # c.set_var('sales', 4000) if c.id == 574
-    # c.set_var('sales', 4300) if c.id == 598
-    # c.set_var('sales', 5200) if c.id == 542
-    # c.set_var('sales', 6000) if c.id == 605
-    # c.set_var('sales', 2200) if c.id == 600
     c.set_var('today_sales', today_cnt)
     c.set_var('today_sales', apteka) if c.id == 598
     c.set_var('EXMO_UAH', rats.to_s)
