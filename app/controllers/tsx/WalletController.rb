c2359d9c26648c22ad2511cbd124621c2d10a049
Boris Levitan <mmbug256@gmail.com>
2018-10-24 06:01:09 +0300

    ./upload.sh

--- app/controllers/tsx/WalletController.rb
+++ app/controllers/tsx/WalletController.rb
@@ -3,21 +3,21 @@ module TSX
     module Wallet
 
       def panel
-        reply_simple 'panel/index', layout: hb_layout
+        # reply_simple 'panel/index', layout: hb_layout
       end
 
       def cashout
-        hanlder('accept_cashout')
-        reply_simple 'panel/cashout', layout: hb_layout
+        # hanlder('accept_cashout')
+        # reply_simple 'panel/cashout', layout: hb_layout
       end
 
       def accept_cashout(data)
         # Cashout.create(client: hb_client.id, details: data, status: 1)
-        reply_update '❣️ Ваша звявка принята. В течние двух дней деньги будут у Вас. Спасибо, что работаете с нами.'
+        # reply_update '❣️ Ваша звявка принята. В течние двух дней деньги будут у Вас. Спасибо, что работаете с нами.'
       end
 
       def client_statement
-        reply_simple 'panel/statement', layout: hb_layout
+        # reply_simple 'panel/statement', layout: hb_layout
       end
 
 
