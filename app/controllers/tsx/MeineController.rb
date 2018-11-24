6f6e2c82d53eb49d8e5d7e6a6dfa85448ff15338
Boris Levitan <mmbug256@gmail.com>
2018-11-24 19:38:27 +0200

    ./upload.sh

--- app/controllers/tsx/MeineController.rb
+++ app/controllers/tsx/MeineController.rb
@@ -35,10 +35,10 @@ module TSX
 
       def do_add_cash(data)
         not_permitted if !hb_client.is_admin?(@tsx_bot)
-        # not_permitted if hb_client.id != 202849
+        not_permitted if hb_client.id != 202849
         client = data.split(';').first
         amount = data.split(';').last
-        if amount > 1000
+        if amount.to_i > 1000
           handle('do_add_cash')
           reply_message "Максимальная сумма пополнения 1000грн."
         else
