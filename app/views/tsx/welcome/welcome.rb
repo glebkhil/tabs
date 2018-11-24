5bcf9a6d45e7ef40d937b90500c48810f3d73821
Boris Levitan <mmbug256@gmail.com>
2018-11-24 14:14:54 +0200

    ./upload.sh

--- app/views/tsx/welcome/welcome.rb
+++ app/views/tsx/welcome/welcome.rb
@@ -4,7 +4,7 @@
 Клиент #{icon('id')} *#{hb_client.id}*
 Баланс *#{@tsx_bot.amo(hb_client.available_cash)}*
 Покупок *#{hb_client.buy_trades([Trade::FINALIZED, Trade::FINISHED]).count}* на *#{@tsx_bot.amo(hb_client.buy_trades([Trade::FINALIZED, Trade::FINISHED]).sum(:price))}* #{"\nПродаж *#{hb_client.sell_trades([Trade::FINALIZED, Trade::FINISHED]).count}* на *#{@tsx_bot.amo(hb_client.sell_trades([Trade::FINALIZED, Trade::FINISHED]).sum(:price))}*" if @sh}
-Рефералов *#{ludey(hb_client.client_referals)}
+Рефералов *#{ludey(hb_client.client_referals)}*
 Заработано *#{@tsx_bot.amo(hb_client.ref_cash)}*
 #{"Поддержка" if !@tsx_bot.support.nil?} #{@tsx_bot.support_line if !@tsx_bot.support.nil?}
 ****
